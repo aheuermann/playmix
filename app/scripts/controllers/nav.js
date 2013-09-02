58d0e49ab0b6ce4376ea60d36c79fe404274acc8
Andrew Heuermann <aheuermann@gmail.com>
2013-09-02 13:30:36 -0500

    Next and prev working

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -9,38 +9,53 @@ app.controller('NavCtrl', function ($scope, $rootScope, Rdio) {
     });
   }
 })
-.controller('PlayerCtrl', function($scope, $rootScope) {
-  console.log("player");
-  
-
+.controller('PlayerCtrl', function($scope, $rootScope, SoundCloud) {
   $rootScope.$watch('playlist', function(newVal, oldVal) {
-    console.log("newVal", newVal);
+    console.log("Playlist", newVal);
     if (!_.isEmpty($rootScope.playlist)){
-      console.log("playlist", $rootScope.playlist);
-      $scope.track = $rootScope.playlist.tracks[0];
+      $rootScope.trackIndex = 0;//Math.floor(Math.random()*$rootScope.playlist.tracks.length);
+      updateTrack();
     }
   });
 
-
-  SC.stream('/tracks/78167093', function(sound){
-    $scope.playing = true;
-    console.log("sound", sound);
-    $scope.sound = sound;
-    console.log("sound", $scope.sound);
-    //sound.play();
-    $rootScope.$apply();
+  $rootScope.$watch('track', function(){
+    console.log("track", $rootScope.track);
+    if($rootScope.track){
+      SoundCloud.remix($rootScope.track).then(function(tracks){
+        if(!_.isEmpty(tracks)){
+          $rootScope.remix = tracks[Math.floor(Math.random()*tracks.length)];
+        }else{
+          console.log("NO REMIXES FOUND");
+        }
+      });
+    }
   });
+
+  $rootScope.$watch('remix', function(){
+    if($rootScope.remix){
+      $scope.play();
+    }
+  });
+
+  var updateTrack = function(){
+    if($scope.sound) $scope.sound.stop();
+    $rootScope.track = $rootScope.playlist.tracks[$rootScope.trackIndex];
+  }
 
   $scope.isPlaying = function() {
     if (!$scope.sound) return false;
     return $scope.sound.playState === 1 && !$scope.sound.paused;
   }
+
   $scope.next = function() {
-    console.log("next");
+    $rootScope.trackIndex = ($rootScope.trackIndex + 1) % $rootScope.playlist.tracks.length;
+    console.log("newIndex", $rootScope.trackIndex);
+    updateTrack();
   }
 
   $scope.prev = function() {
-    console.log("prev");
+    $rootScope.trackIndex = ($rootScope.trackIndex - 1 + $rootScope.playlist.tracks.length) % $rootScope.playlist.tracks.length;
+    updateTrack();
   }
 
   $scope.togglePlay = function() {
@@ -48,9 +63,21 @@ app.controller('NavCtrl', function ($scope, $rootScope, Rdio) {
     if ($scope.sound){
       //$scope.sound.togglePause();
     }else{
+      $scope.play();
       console.log("no sound");
     }
     console.log("sound", $scope.sound);
   }
 
+  $scope.play = function() {
+    if ($scope.remix){
+      console.log("remix", $scope.remix);
+      SC.stream('/tracks/' + $scope.remix.id, function(sound){
+        console.log("playing");
+        $scope.sound = sound;
+        sound.play();
+        $rootScope.$apply();
+      });
+    }
+  }
 });
