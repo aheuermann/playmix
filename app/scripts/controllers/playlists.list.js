f530665146e8e8c9bd1fbabf4dea360901d9206a
Andrew Heuermann <aheuermann@gmail.com>
2013-09-06 23:34:48 -0500

    Include other types of playlist, would like to add user icon but it is not in the payload for playlist, unfortunately

--- app/scripts/controllers/playlists.list.js
+++ app/scripts/controllers/playlists.list.js
@@ -2,7 +2,8 @@
 
 app.controller('PlaylistsCtrl', function ($scope, Rdio, Player) {
   Rdio.getPlaylists().then(function(playlists){
-    $scope.playlists = playlists.owned;
+    console.log("playlists", playlists);
+    $scope.playlists = playlists.owned.concat(playlists.subscribed, playlists.collab);
   });
   $scope.queue = function(playlist){
     Player.lookupAndPlay(playlist.tracks);
