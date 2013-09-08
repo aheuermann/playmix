c44fd9d50752bc10293bfc8037b34f1cba12a018
Andrew Heuermann <aheuermann@gmail.com>
2013-09-08 13:03:44 -0500

    Fixing navbar height with alert issue.

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,8 +1,10 @@
 'use strict';
 
-app.controller('NavCtrl', function ($scope, $rootScope, Rdio) {
+app.controller('NavCtrl', function ($scope, $rootScope, Rdio, Alert) {
   $scope.signIn = function() {
+    Alert.info("Logging in...");
     Rdio.auth().then(function(user) {
+      Alert.clear();
       $rootScope.user = user;
     }, function(){
       console.log("eerrrrrrr");
